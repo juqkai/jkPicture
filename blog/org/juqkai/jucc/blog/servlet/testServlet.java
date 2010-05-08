c77ade66ee7e858285bb81af3c02e9c671e15c2b
juqkai <juqkai@f7331dd8-63cf-4af0-d107-c0c28e746004>
2010-05-08 09:30:47 +0000

    终于将SPRING MVC搞进来了,但是对配置文件的位置不满意

--- blog/org/juqkai/jucc/blog/servlet/testServlet.java
+++ blog/org/juqkai/jucc/blog/servlet/testServlet.java
@@ -1,11 +1,8 @@
 package org.juqkai.jucc.blog.servlet;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class testServlet {
